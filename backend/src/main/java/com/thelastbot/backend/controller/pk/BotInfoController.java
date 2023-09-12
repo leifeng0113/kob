2a79365d3e418a1b804f8b8a7de96b1241579d9c
leifeng <leifeng0113@gmail.com>
2023-09-13 00:58:39 +0800

    搭建好了项目骨架

--- backend/src/main/java/com/thelastbot/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/thelastbot/backend/controller/pk/BotInfoController.java
@@ -4,15 +4,26 @@ package com.thelastbot.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
 @RestController
 
 @RequestMapping("/pk/")
 public class BotInfoController {
 
 
-    @RequestMapping("/getBotInfo/")
-public String getBotInfo(){
-    return "the bot";
+
+
+@RequestMapping("getbotinfo/")
+public Map<String,String> getBotInfo(){
+    Map<String,String>bot1=new HashMap<>();
+    bot1.put("name","saber");
+    bot1.put("rating","1500");
+    return bot1;
+
+    }
 }
 
-}
