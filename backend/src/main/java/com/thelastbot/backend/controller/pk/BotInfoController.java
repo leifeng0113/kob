e42b058ca7ea7254b39036ec353242e311e14d97
leifeng <leifeng0113@gmail.com>
2023-09-12 18:16:08 +0800

    创建后端

---
+++ backend/src/main/java/com/thelastbot/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,18 @@
+package com.thelastbot.backend.controller.pk;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/pk/")
+public class BotInfoController {
+
+
+    @RequestMapping("/getBotInfo/")
+public String getBotInfo(){
+    return "the bot";
+}
+
+}
